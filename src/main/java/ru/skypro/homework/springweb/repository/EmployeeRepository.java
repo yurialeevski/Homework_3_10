7998616b0bcdeda6d6d144156b6e1f7290adcf5f
ya121 <ya7771@yandex.ru>
2023-06-04 14:54:37 +0300

    Сделано ДЗ № 3_10 Spring Web

--- src/main/java/ru/skypro/homework/springweb/repository/EmployeeRepository.java
+++ src/main/java/ru/skypro/homework/springweb/repository/EmployeeRepository.java
@@ -10,4 +10,10 @@ public interface EmployeeRepository {
     int getMinSalary();
     int getMaxSalary();
     List<Employee> getHighSalary();
+    List<Employee> getHigherThanSalary(Integer salary);
+    void updateById(Integer id, Employee employee);
+    void addEmployee(Employee employee);
+    void addListOf(List<Employee> employees);
+    Employee getById(Integer id);
+    void deleteById(Integer id);
 }
